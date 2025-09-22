0cc85d585b0f561914b20b14efb58906a7a15a3e
VigKillian <viguier.killian05@gmail.com>
2025-09-22 12:59:48 +0200

    Fix clamping cam

--- scenes/perso.gd
+++ scenes/perso.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var previous_mouse_pos:Vector2 = DisplayServer.window_get_size()/2
-@onready var cam_fps = $Camera3D
+@onready var cam_fps :Node3D = $Camera3D
 var cam_speed = 0.5
 
 
@@ -22,16 +22,25 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (cam_fps.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
-	cam_fps.global_rotation.x -= cam_diff.y * delta * cam_speed
-	cam_fps.global_rotation.x = deg_to_rad(clamp(rad_to_deg(cam_fps.global_rotation.x), -89.9, 89.9))
+	
+	if(rad_to_deg(cam_fps.global_rotation.x - cam_diff.y * delta * cam_speed) < -80):
+		print("C'est moins")
+		cam_fps.global_rotation.x = deg_to_rad(-79.9)
+	elif(rad_to_deg(cam_fps.global_rotation.x - cam_diff.y * delta * cam_speed) > 80):
+		print(rad_to_deg(cam_fps.global_rotation.x))
+		cam_fps.global_rotation.x = deg_to_rad(80)
+	else:
+		cam_fps.global_rotation.x -= cam_diff.y * delta * cam_speed
+
+	
 	cam_fps.global_rotation.y -= cam_diff.x * delta * cam_speed
 	
 	if get_viewport().get_window().has_focus():
