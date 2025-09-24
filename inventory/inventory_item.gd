ef86d08aa63a2446e4f231545d37de04469cfc69
Mansour Andrew <andrew.mansour@etu.umontpellier.fr>
2025-09-24 13:15:58 +0200

    Start of inventory system

---
+++ inventory/inventory_item.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name InvItem
+
+@export var name : String = ""
+@export var texture : Texture2D # or texture
+@export var tags : Array[String] #if tags are specific things create a class and replace String with Tags class
+@export var description : String = "" # Optional
--- scripts/perso.gd
+++ scripts/perso.gd
@@ -30,7 +30,7 @@ func _physics_process(delta: float) -> void:
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (cam_fps.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	direction = -(direction.cross(Vector3.UP)).cross(Vector3.UP).normalized()
-	print(direction)
+	#print(direction)
 	
 	if direction:
 		velocity.x = direction.x * SPEED
